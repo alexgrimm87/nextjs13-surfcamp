5c827c485ffda206e8e6dd27411130f85fa5d06d
Oleksandr Grymashevych <alexgrimm87@gmail.com>
2024-01-27 21:48:25 +0200

    info block

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import HeroSection from "@/app/_components/HeroSection";
+import InfoBlock from "@/app/_components/InfoBlock";
 
 export default function Home() {
   const heroHeadline = (
@@ -10,9 +11,35 @@ export default function Home() {
     </>
   );
 
+  const infoBlockData = {
+    headline: "the experience.",
+    text: (
+      <p className="copy">
+        At Sam’s Surfcamp, we invite you to embark on an unforgettable surfing
+        adventure. Nestled in the heart of [Location] our surf camp offers an
+        exhilarating experience for beginners, intermediate surfers, and
+        seasoned wave riders alike. Dive into the world of surfing with our
+        expert instructors who have years of experience and a deep passion for
+        the sport. Whether you're a first-time surfer looking to catch your
+        first wave or a seasoned pro seeking to enhance your skills, our
+        dedicated team is here to guide you every step of the way. Immerse
+        yourself in the natural beauty of our surf camp's surroundings. Picture
+        yourself waking up to the sound of crashing waves and feeling the warm
+        sand beneath your feet. With pristine beaches and a vibrant coastal
+        atmosphere, [Location] sets the perfect stage for your surf adventure.
+      </p>
+    ),
+    button: (
+      <button className="btn btn--medium btn--turquoise">Book now</button>
+    ),
+    showImageRight: false
+  };
+
   return (
     <main>
       <HeroSection headline={heroHeadline} />
+      <InfoBlock data={infoBlockData} />
+      <InfoBlock data={{ ...infoBlockData, showImageRight: true }} />
     </main>
   );
 }
