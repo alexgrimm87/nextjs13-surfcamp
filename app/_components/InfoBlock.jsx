c7ab1a35380547ab8cc04a281742d212426b3152
Oleksandr Grymashevych <alexgrimm87@gmail.com>
2024-01-28 16:22:06 +0200

    display info blocks with strapi data

--- app/_components/InfoBlock.jsx
+++ app/_components/InfoBlock.jsx
@@ -1,3 +1,5 @@
+import ReactMarkdown from "react-markdown";
+
 const InfoBlock = ({ data }) => {
   const { headline, text, showImageRight, imageSrc, button } = data;
 
@@ -10,7 +12,7 @@ const InfoBlock = ({ data }) => {
       />
       <div className="info__text">
         <h2 className="info__headline">{headline}</h2>
-        {text}
+        <ReactMarkdown className="copy">{text}</ReactMarkdown>
         {button}
       </div>
     </div>
---
+++ utils/strapi.utils.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+import Link from "next/link";
+
+const BASE_URL = process.env.STRAPI_URL || "http://127.0.0.1:1337";
+
+export async function fetchDataFromStrapi(route) {
+  const url = `${BASE_URL}/api/${route}`;
+
+  try {
+    const response = await axios.get(url);
+    return response.data.data;
+  } catch (err) {
+    console.log(err);
+    throw new Error(`Could not fetch data from ${url}`);
+  }
+}
+
+export function processInfoBlocks(data) {
+  const infoBlocksRaw = data.attributes.info_blocks.data;
+
+  return infoBlocksRaw.map((infoBlock) => ({
+    ...infoBlock.attributes,
+    imageSrc: BASE_URL + infoBlock.attributes?.image?.data?.attributes?.url,
+    id: infoBlock.id,
+    button: createInfoBlockButton(infoBlock.attributes.button)
+  }));
+}
+
+function createInfoBlockButton(buttonData) {
+  if (!buttonData) {
+    return null;
+  }
+
+  return (
+    <Link
+      href={`/${buttonData.slug}`}
+      className={`btn btn--medium btn--${buttonData.colour}`}
+    >
+      {buttonData.text}
+    </Link>
+  );
+}
