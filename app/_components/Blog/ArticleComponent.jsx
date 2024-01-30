10a043f981db291e2aee5dedd7af76c12973e6db
Oleksandr Grymashevych <alexgrimm87@gmail.com>
2024-01-30 21:02:17 +0200

    article component add paragraph

--- app/_components/Blog/ArticleComponent.jsx
+++ app/_components/Blog/ArticleComponent.jsx
@@ -1,5 +1,6 @@
 import ArticleHeadline from "@/app/_components/Blog/ArticleHeadline";
 import ImageTextComponent from "@/app/_components/Blog/ImageTextComponent";
+import ArticleParagraph from "@/app/_components/Blog/ArticleParagraph";
 
 const ArticleComponent = ({ component }) => {
   const componentType = component.__component.split("blog-article.")[1];
@@ -10,7 +11,7 @@ const ArticleComponent = ({ component }) => {
     case "paragraph-with-image":
       return <ImageTextComponent component={component} />;
     case "paragraph":
-      return <h1>Paragraph Component</h1>;
+      return <ArticleParagraph paragraph={component} />;
     case "landscape-image":
       return <h1>Landscape Image</h1>;
     default:
