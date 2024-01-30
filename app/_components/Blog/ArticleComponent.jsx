a065bed60fe6ca84b52d2354ca4a529ab4c32542
Oleksandr Grymashevych <alexgrimm87@gmail.com>
2024-01-30 21:15:31 +0200

    article component add landscape image

--- app/_components/Blog/ArticleComponent.jsx
+++ app/_components/Blog/ArticleComponent.jsx
@@ -1,6 +1,7 @@
 import ArticleHeadline from "@/app/_components/Blog/ArticleHeadline";
 import ImageTextComponent from "@/app/_components/Blog/ImageTextComponent";
 import ArticleParagraph from "@/app/_components/Blog/ArticleParagraph";
+import LandscapeImage from "@/app/_components/Blog/LandscapeImage";
 
 const ArticleComponent = ({ component }) => {
   const componentType = component.__component.split("blog-article.")[1];
@@ -13,7 +14,7 @@ const ArticleComponent = ({ component }) => {
     case "paragraph":
       return <ArticleParagraph paragraph={component} />;
     case "landscape-image":
-      return <h1>Landscape Image</h1>;
+      return <LandscapeImage imageData={component} />;
     default:
       return <h1>Component not found</h1>;
   }
