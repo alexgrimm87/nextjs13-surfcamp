7f7b4f17f130946bf41b29532201218b077a2415
Oleksandr Grymashevych <alexgrimm87@gmail.com>
2024-01-31 17:06:42 +0200

    event page with signup form

---
+++ app/_components/Events/SignupForm.jsx
@@ -0,0 +1,96 @@
+"use client";
+import axios from "axios";
+import {useState} from "react";
+import {allDataFilledIn} from "@/utils/validation.utils";
+import TextInput from "@/app/_components/TextInput";
+
+const SignupForm = ({ headline, infoText, buttonLabel }) => {
+  const [formData, setFormData] = useState({
+    firstName: "Niklas",
+    lastName: "Fischer",
+    email: "nik@gmail.com",
+    phone: "321321"
+  });
+
+  const [showConfirmation, setShowConfirmation] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(false);
+
+  const onChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    const payload = {
+      data: { ...formData, isGeneralInterest: true }
+    };
+
+    if (allDataFilledIn(formData)) {
+      try {
+        const response = await axios.post(
+          `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/participants`,
+          payload
+        );
+        setShowConfirmation(true);
+      } catch (err) {
+        setErrorMessage(
+          err.response?.data?.error?.message || "Something went wrong"
+        );
+      }
+    } else {
+      setErrorMessage("Please fill out all fields.");
+    }
+  };
+
+  return (
+    <section className="signup-form">
+      <div className="signup-form__info">
+        <h3 className="signup-form__headline">{headline}</h3>
+        {infoText}
+      </div>
+      {showConfirmation ? (
+        <div className="signup-form__form">
+          <h4>Thank you for signing up. We will get in touch soon!</h4>
+        </div>
+      ) : (
+        <form className="signup-form__form" onSubmit={onSubmit}>
+          <div className="signup-form__name-container">
+            <TextInput
+              inputName="firstName"
+              value={formData.firstName}
+              onChange={onChange}
+              label="First Name"
+            />
+            <TextInput
+              inputName="lastName"
+              value={formData.lastName}
+              onChange={onChange}
+              label="Last Name"
+            />
+          </div>
+          <TextInput
+            inputName="email"
+            value={formData.email}
+            onChange={onChange}
+            label="Your email address"
+          />
+          <TextInput
+            inputName="phone"
+            value={formData.phone}
+            onChange={onChange}
+            label="Your telephone number"
+          />
+          {errorMessage && (
+            <p className="copy signup-form__error">{errorMessage}</p>
+          )}
+          <button className="btn btn--medium btn--turquoise" type="submit">
+            {buttonLabel || "Stay in touch!"}
+          </button>
+        </form>
+      )}
+    </section>
+  );
+};
+
+export default SignupForm;
